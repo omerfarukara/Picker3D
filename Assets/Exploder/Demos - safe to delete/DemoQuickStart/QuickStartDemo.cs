d99b8967c4c1337766cc2f46c88ec09c22b541d6
Ömer Faruk Kara <o.f.karaa@gmail.com>
2023-12-09 11:05:06 +0300

    Initial commit

---
+++ Assets/Exploder/Demos - safe to delete/DemoQuickStart/QuickStartDemo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Exploder.Utils;
+
+namespace Exploder.Demo
+{
+    public class QuickStartDemo : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> objects;
+
+        private void OnGUI()
+        {
+            if (GUI.Button(new Rect(10, 10, 120, 70), "Explode sphere"))
+            {
+                ExploderSingleton.Instance.ExplodeObjects(objects.ToArray());
+                foreach (var obj in objects)
+                {
+                    obj.SetActive(false);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/[---Picker3D---]/Scripts/Helpers/Contants.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Contants
+{
+    public static class Tags
+    {
+        public const string PLAYER = "Player";
+    }
+}
